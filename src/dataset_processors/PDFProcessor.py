0b49c2b8182922c96ceac68a429d6684aa068490
AMER\kevin.freistroffer <kevin.freistroffer@gmail.com>
2025-04-26 10:20:44 -0700

    delete todo comment

--- src/dataset_processors/PDFProcessor.py
+++ src/dataset_processors/PDFProcessor.py
@@ -3,7 +3,6 @@ import os
 from PyPDF2 import PdfReader
 
 
-# TODO implement a BaseProcessor requiring these method names?
 class PDFProcessor:
     def __init__(self, file_name):
         print("Initializing PDFProcessor...")
